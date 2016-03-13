1f71b63065efe68fb44129c5e1a332f2be243997
zavakid <zava.kid@gmail.com>
2016-03-13 20:24:07 +0800

    minor update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-git checkout coding-pages && rsync -r output/ .  && git ci -m "deploy" && git checkout master
+git ci . --m "minor update" &&
+  git checkout coding-pages &&
+  rsync -r output/ .  &&
+  git ci . -m "deploy" &&
+  git checkout master
